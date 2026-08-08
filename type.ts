c33f0549e4c472c92f1f1cdc5df87b6068a4ae87
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-08-09 00:52:28 +0300

    Add types

--- type.ts
+++ type.ts
@@ -2,3 +2,29 @@ export interface IFormInput {
   FirstName: string;
   LastName: string;
 }
+
+export interface DataCardProps {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+export interface UserData {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  isActive: boolean;
+}
+
+export interface PaginationProps {
+  pages: number;
+  setPagination: (page: number) => void;
+  pagination: number;
+}
+
+export interface TableProps {
+  data: UserData[];
+  tableTitle?: string;
+}
